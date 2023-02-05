65c76afcc49d9dbbbedf48ea29e822b04e24f080
mindcrazy <123137817+mindcrazy@users.noreply.github.com>
2023-02-05 19:42:27 -0300

    Create root.js

---
+++ js/root.js
@@ -0,0 +1,4 @@
+import { navbar } from  "./navbar.js";
+import { slideshow } from  "./slideshow.js";
+document.getElementById("navbar").innerHTML = navbar;
+document.getElementById("slideshow").innerHTML = slideshow;
