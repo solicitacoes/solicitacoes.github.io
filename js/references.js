e312b0c98677e71893741c8b0590464d0a1b6a13
mindcrazy <123137817+mindcrazy@users.noreply.github.com>
2023-02-06 02:21:17 -0300

    Create references.js

---
+++ js/references.js
@@ -0,0 +1,14 @@
+export const URL1 = `<div class="stepwizard-step">
+<a href="#step-1" type="button" class="btn btn-primary btn-circle">1</a>
+<p>Processo</p>
+</div>`
+
+export const URL2 = `<div class="stepwizard-step">
+<a href="#step-2" type="button" class="btn btn-default btn-circle" disabled="disabled">2</a>
+<p>Análise</p>
+</div>`
+
+export const URL3 = `<div class="stepwizard-step">
+<a href="#step-3" type="button" class="btn btn-default btn-circle" disabled="disabled">3</a>
+<p>Confirmação</p>
+</div>`
