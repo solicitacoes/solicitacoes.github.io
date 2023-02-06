780fd634656a71c798adccfc5f4fdc007d39a0c8
mindcrazy <123137817+mindcrazy@users.noreply.github.com>
2023-02-06 03:57:54 -0300

    Update api_ajax_get_estabelecimento.js

--- js/api_ajax_get_estabelecimento.js
+++ js/api_ajax_get_estabelecimento.js
@@ -1,6 +1,24 @@
 const http = new simpleAJAX;
 
-users = http.get('https://jsonplaceholder.typicode.com/q/estabelecimento',
+export const TemplateEstabelecimento = http.get('https://jsonplaceholder.typicode.com/solicitar/estabelecimentos/template',
+  (err, users) => {
+    if(err) {
+      console.log(err) // { nome:'', email:'', telefone:'', whatsapp:'', categorias:'', endereço:'' }
+    } else {
+    console.log(users); // { nome:'', email:'', telefone:'', whatsapp:'', categorias:'', endereço:'' }
+    }
+  });
+
+export const EstabelecimentosComEmail = http.get('https://jsonplaceholder.typicode.com/solicitar/estabelecimentos/email',
+  (err, users) => {
+    if(err) {
+      console.log(err) // { nome:'', email:'', telefone:'', whatsapp:'', categorias:'', endereço:'' }
+    } else {
+    console.log(users); // { nome:'', email:'', telefone:'', whatsapp:'', categorias:'', endereço:'' }
+    }
+  });
+
+export const EstabelecimentosComWhatsapp = http.get('https://jsonplaceholder.typicode.com/solicitar/estabelecimentos/whatsapp',
   (err, users) => {
     if(err) {
       console.log(err) // { nome:'', email:'', telefone:'', whatsapp:'', categorias:'', endereço:'' }
