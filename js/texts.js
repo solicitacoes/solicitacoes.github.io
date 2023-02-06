d3ea35bd438c5470b899b697f0b90ef2d860f5a8
mindcrazy <123137817+mindcrazy@users.noreply.github.com>
2023-02-06 04:20:39 -0300

    Update texts.js

--- js/texts.js
+++ js/texts.js
@@ -1,3 +1,6 @@
+// page: 0
+export const text0 = `Utilizamos coisas como localstorage/session para enviar dados entre as páginas, isso evita ter que preencher a página novamente em caso de alguma falha de conexão ou problema geral. Está ciente ao confirmar isso?`
+
 // page: Solicitar itens, serviços ou doações
 export const text1 = `Devido à pandemia causado pelo coronavírus espalhando o covid-19 mundo afora, profissionais de saúde e governantes recomendam que as pessoas fiquem em quarentena e que se mantenha um distanciamento social.
 Porém, ainda precisamos de itens, serviços, e em alguns casos, doações. Com a necessidade informada acima foi disponibilizado esse site para conectar pessoas e estabelecimentos virtualmente. 
@@ -24,3 +27,24 @@ export const text4  = `Estabelecimento que deseja se cadastrar no sistema favor 
 Todo a solicitação de cadastro será realizada uma confirmação.
 Será encaminhado um retorno quando o seu cadastro estiver disponível.
 Se você é um desenvolvedor de software ajude a automatizar esse cadastro.`.trim();
+
+// page: 5
+export const text5 = `Estamos salvando a informação de cadastro em nosso servidor de forma externa e remota. Pode aguardar um pouco?`
+
+// page: 6
+export const text6 = `Estamos enviando uma cópia de seus dados temporariamente nesse link. Pode aguardar um pouco?`
+
+// page: 7
+export const text7 = `cópia local interna de cadastro gerada`
+
+// page: 8
+export const text8 = `cópia remota do cadastro gerado`
+
+// page: 9
+export const text9 = `cópia do arquivo para o acesso público gerado`
+
+// page: 10
+export const text10 = `cópia do arquivo para o acesso temporário gerado. compartilhe esse link de cadastro para uma ou mais pessoas caso queira`
+
+// page: 11
+export const text11 = `cópia local externa de cadastro realizado com sucesso, pdf gerado`
