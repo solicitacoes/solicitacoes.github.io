ccd0fc2b7459d1a061918d437eef467f69abdfb8
mindcrazy <123137817+mindcrazy@users.noreply.github.com>
2023-02-06 02:29:51 -0300

    Update textareas.js

--- js/textareas.js
+++ js/textareas.js
@@ -1,3 +1,5 @@
+import { text1, text2 } from "./texts.js"
+
 export const styleTextArea = `height: 121px; border: none; width: 100%; background: none; text-align: left;`;
 export const textarea1 = `<textarea style="${styleTextArea}"> ${text1} </textarea>`.trim();
 export const textarea2 = `<textarea style="${styleTextArea}"> ${text2} </textarea>`
