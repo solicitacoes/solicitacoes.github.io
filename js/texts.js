361718227017e25ff90fd042807ee0b72d4aefa1
mindcrazy <123137817+mindcrazy@users.noreply.github.com>
2023-02-06 04:11:59 -0300

    Update texts.js

--- js/texts.js
+++ js/texts.js
@@ -1,8 +1,26 @@
+// page: Solicitar itens, serviços ou doações
 export const text1 = `Devido à pandemia causado pelo coronavírus espalhando o covid-19 mundo afora, profissionais de saúde e governantes recomendam que as pessoas fiquem em quarentena e que se mantenha um distanciamento social.
 Porém, ainda precisamos de itens, serviços, e em alguns casos, doações. Com a necessidade informada acima foi disponibilizado esse site para conectar pessoas e estabelecimentos virtualmente. 
 Precisa comprar pão, solicite a uma padaria ou mercado. 
 Você dono de estabelecimento, utilize esse site para receber encomendas. 
 Dessa forma evitamos superfaturamentos de plataformas de entregas.
-E ainda ajudamos os pequenos comerciantes. Portanto, fique em casa!`
+E ainda ajudamos os pequenos comerciantes. Portanto, fique em casa!`.trim();
 
-export const text2  = `Devido à pandemia causado pelo coronavírus espalhando o covid-19 mundo afora, profissionais de saúde e governantes recomendam que as pessoas fiquem em quarentena e que se mantenha um distanciamento social. Porém, ainda precisamos de itens, serviços, e em alguns casos, doações. Com a necessidade informada acima foi disponibilizado esse site para conectar pessoas e estabelecimentos virtualmente. Precisa comprar pão, solicite a uma padaria ou mercado. Você dono de estabelecimento, utilize esse site para receber encomendas. Dessa forma evitamos superfaturamentos de plataformas de entregas. E ainda ajudamos os pequenos comerciantes. Portanto, fique em casa!`
+// page: Solicitar itens, serviços ou doações
+export const text2  = `Devido à pandemia causado pelo coronavírus espalhando o covid-19 mundo afora, profissionais de saúde e governantes recomendam que as pessoas fiquem em quarentena e que se mantenha um distanciamento social. 
+Porém, ainda precisamos de itens, serviços, e em alguns casos, doações. Com a necessidade informada acima foi disponibilizado esse site para conectar pessoas e estabelecimentos virtualmente. 
+Precisa comprar pão, solicite a uma padaria ou mercado. 
+Você dono de estabelecimento, utilize esse site para receber encomendas. 
+Dessa forma evitamos superfaturamentos de plataformas de entregas.
+E ainda ajudamos os pequenos comerciantes. Portanto, fique em casa!`.trim();
+
+// page: Seus dados
+export const text2  = `Aqui você deve informar seus dados para que o estabelecimento entre em contato.
+Seus dados serão armazenados apenas neste computador e não serão disponibilizados.
+Caso esteja utilizando um computador público recomenda-se utilizar o modo privado do navegador ou após a utilização limpar os dados do navegador.`
+
+// page: Cadastrar estabelecimento
+export const cadastro  = `Estabelecimento que deseja se cadastrar no sistema favor preencher esse formulário.
+Todo a solicitação de cadastro será realizada uma confirmação.
+Será encaminhado um retorno quando o seu cadastro estiver disponível.
+Se você é um desenvolvedor de software ajude a automatizar esse cadastro.`
