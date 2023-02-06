a015dc889935de04ccf903b27c8dae5fc28c7faa
mindcrazy <123137817+mindcrazy@users.noreply.github.com>
2023-02-06 02:20:21 -0300

    Create forms.js

---
+++ js/forms.js
@@ -0,0 +1,14 @@
+export const Form1 = `<div class="form-group">
+<label class="control-label">Nome</label>
+<input maxlength="200" type="text" required="required" class="form-control" placeholder="Inserir nome completo">
+</div>`
+
+export const Form2 = `<div class="form-group">
+<label class="control-label">Celular</label>
+<input maxlength="200" type="text" required="required" class="form-control" placeholder="Inserir número de celular">
+</div>`
+
+export const Form3 = `<div class="form-group">
+<label class="control-label">E-mail</label>
+<input maxlength="200" type="text" required="required" class="form-control" placeholder="Inserir endereço de e-mail completo">
+</div>`
