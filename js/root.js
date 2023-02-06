e876c57d4c0a8f2b9bbb2f5288092714da942263
mindcrazy <123137817+mindcrazy@users.noreply.github.com>
2023-02-06 02:06:37 -0300

    Update root.js

--- js/root.js
+++ js/root.js
@@ -1,4 +1,5 @@
 import { navbar } from  "./navbar.js";
-import { slideshow } from  "./slideshow.js";
+//import { slideshow } from  "./slideshow.js";
+import { slideshow } from  "./testSlideshow.js";
 document.getElementById("navbar").innerHTML = navbar;
 document.getElementById("slideshow").innerHTML = slideshow;
