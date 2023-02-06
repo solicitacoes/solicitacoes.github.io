432f68b0cd122e6be1b4a090e65d2e224499a087
mindcrazy <123137817+mindcrazy@users.noreply.github.com>
2023-02-06 02:23:02 -0300

    Create buttons.js

---
+++ js/buttons.js
@@ -0,0 +1,12 @@
+export const Buttons2 = `<p style="margin-top: 20px;">
+<button class="btn btn-primary prevBtn btn-lg pull-left" type="button">Voltar</button>
+<a class="btn btn-primary prevBtn btn-lg pull-left" href="/estabelecimento">Selecionar estabelecimento</a>
+<button class="btn btn-success btn-lg pull-right" type="submit">Confirmar</button>
+</p>`
+
+export const ConfirmarButton = `<p> <button class="btn btn-success nextBtn btn-lg pull-right" type="button" style="width: 100%;"> Confirmar </button> </p>`
+
+export const AllButtons = `<p style="margin-top: 20px;">
+<button class="btn btn-primary prevBtn btn-lg pull-left" type="button">Voltar</button>
+<button class="btn btn-danger nextBtn btn-lg pull-right" type="button">Confirmar</button>
+</p>`
