685da5b2c15375548d7d9aa39086403b2eb7f2a1
mindcrazy <123137817+mindcrazy@users.noreply.github.com>
2023-02-06 03:11:28 -0300

    Create api_tmpfiles.js

---
+++ js/api_tmpfiles.js
@@ -0,0 +1,23 @@
+/*
+/TMP/FILES
+TEMPORARY FILE HOSTING
+You can use our API to automate file uploads.
+
+All uploaded files are automatically deleted after 60 minutes.
+
+
+Method: POST
+
+Params: file=/path/to/test.jpg
+
+URL: https://tmpfiles.org/api/v1/upload
+
+
+Example with CURL:
+
+curl -F "file=@/Users/myuser/test.jpg" https://tmpfiles.org/api/v1/upload
+
+Upload
+ 
+API
+*/
