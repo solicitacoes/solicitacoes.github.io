96999d5d7b4fef5be020ceb7f419ebcfad40fbfc
mindcrazy <123137817+mindcrazy@users.noreply.github.com>
2023-02-06 02:40:43 -0300

    Update testSlideshow.js

--- js/testSlideshow.js
+++ js/testSlideshow.js
@@ -5,7 +5,7 @@ import { URL1, URL2, URL3 } from "./references.js"
 import { Buttons2, ConfirmarButton, AllButtons } from "./buttons.js"
 
 export const page1 = "Solicitar itens, serviços ou doações"
-export const pageAbout = `<h4 id="solicitar" style="text-align: center;"> ${page1} </h4>`
+export const pageAbout = `<h4 id="solicitar" style="text-align: center;margin-top: 60px;"> ${page1} </h4>`
 export const stepWizards = `<div class="stepwizard-row setup-panel">
       ${URL1}
       ${URL2}
