d11e59557c4f058298cda9f81b2d9fa0cc5ba98a
mindcrazy <123137817+mindcrazy@users.noreply.github.com>
2023-02-06 02:18:01 -0300

    Create texts.js

---
+++ js/texts.js
@@ -0,0 +1,8 @@
+export const text1 = `Devido à pandemia causado pelo coronavírus espalhando o covid-19 mundo afora, profissionais de saúde e governantes recomendam que as pessoas fiquem em quarentena e que se mantenha um distanciamento social.
+Porém, ainda precisamos de itens, serviços, e em alguns casos, doações. Com a necessidade informada acima foi disponibilizado esse site para conectar pessoas e estabelecimentos virtualmente. 
+Precisa comprar pão, solicite a uma padaria ou mercado. 
+Você dono de estabelecimento, utilize esse site para receber encomendas. 
+Dessa forma evitamos superfaturamentos de plataformas de entregas.
+E ainda ajudamos os pequenos comerciantes. Portanto, fique em casa!`
+
+export const text2  = `Devido à pandemia causado pelo coronavírus espalhando o covid-19 mundo afora, profissionais de saúde e governantes recomendam que as pessoas fiquem em quarentena e que se mantenha um distanciamento social. Porém, ainda precisamos de itens, serviços, e em alguns casos, doações. Com a necessidade informada acima foi disponibilizado esse site para conectar pessoas e estabelecimentos virtualmente. Precisa comprar pão, solicite a uma padaria ou mercado. Você dono de estabelecimento, utilize esse site para receber encomendas. Dessa forma evitamos superfaturamentos de plataformas de entregas. E ainda ajudamos os pequenos comerciantes. Portanto, fique em casa!`
