389b7c67e7faa311cdba054898d1c308bb632178
mindcrazy <123137817+mindcrazy@users.noreply.github.com>
2023-02-06 03:47:51 -0300

    Update api_ajax_post.js

--- js/api_ajax_post.js
+++ js/api_ajax_post.js
@@ -1,26 +1,9 @@
 const http = new simpleAJAX;
 
 const data = {
-    "name": "Bolaji Ayodeji",
-    "username": "bolajiayodeji",
-    "email": "hi@bolajiayodeji.com",
-    "address": {
-      "street": "Hello world",
-      "suite": "Hello",
-      "city": "Lokoja, Kogi",
-      "zipcode": "xxx-xxx-xxx",
-      "geo": {
-        "lat": "29.4572",
-        "lng": "-164.2990"
-      }
-    },
-    "phone": "+234 8109445504",
-    "website": "https://bolajiayodeji.com",
-    "company": {
-      "name": "PhilanthroLab",
-      "catchPhrase": "Multi-tiered zero tolerance productivity",
-      "bs": "transition cutting-edge web services"
-    }
+    "nome": "Bolaji Ayodeji",
+    "celular": "bolajiayodeji",
+    "email": "hi@bolajiayodeji.com"
 };
 
 http.post('https://jsonplaceholder.typicode.com/users',
