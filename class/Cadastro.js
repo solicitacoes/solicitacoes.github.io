cbd925468e122f12d21d68f8195aa0b5b5ec1dda
mindcrazy <123137817+mindcrazy@users.noreply.github.com>
2023-02-07 02:35:04 -0300

    Update Cadastro.js

--- class/Cadastro.js
+++ class/Cadastro.js
@@ -1,8 +1,15 @@
 export default class Cadastro {
 
     constructor(nome) {
+        const format = /[!@#$%^&*()_+\-=\[\]{};:\\|,.<>\/?]/;
         if (nome === undefined || nome.length <= 0 || nome  === "" || nome === '' ) {
             throw new Error("O nome é obrigatório para uma Pessoa");
+        }
+        if (checkNumber.test(nome) === false) { // nome1 nome 2 nome3 nome4 ... 
+            throw new Error("O nome não pode conter número");
+        }
+        if (format.test(nome) === false) { // "My@string-with(some%text)" or '[!@#$%^&*()_+\-=\[\]{};:\\|,.<>\/?]'
+            throw new Error("O nome não pode símbolo especial como tab, ponto etc - com a excessão do espaço em branco");
         }
         this.nome = nome;
     }
